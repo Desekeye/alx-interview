b8d5a20660526deef25cec64c40e193b0d33863f
Desekeye <106808436+Desekeye@users.noreply.github.com>
2023-06-04 22:42:40 +0200

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -22,26 +22,4 @@ if (process.argv.length > 2) {
       .then(names => console.log(names.join('\n')))
       .catch(allErr => console.log(allErr));
   });
-}#!/usr/bin/node
-
-const request = require('request');
-
-const movieId = process.argv[2];
-
-const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
-
-request(url, async (err, res, body) => {
-  err && console.log(err);
-
-  const charactersArray = (JSON.parse(res.body).characters);
-  for (const character of charactersArray) {
-    await new Promise((resolve, reject) => {
-      request(character, (err, res, body) => {
-        err && console.log(err);
-
-        console.log(JSON.parse(body).name);
-        resolve();
-      });
-    });
-  }
-});
+}
