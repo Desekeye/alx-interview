844d43ec14a8cf779bd213d634a3253b4c771171
Desekeye <106808436+Desekeye@users.noreply.github.com>
2023-06-08 00:48:26 +0200

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,36 @@
 #!/usr/bin/node
+
+const mainUrl = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 const request = require('request');
-const API_URL = 'https://swapi-api.hbtn.io/api';
 
-if (process.argv.length > 2) {
-  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
-    if (err) {
-      console.log(err);
-    }
-    const charactersURL = JSON.parse(body).characters;
-    const charactersName = charactersURL.map(
-      url => new Promise((resolve, reject) => {
-        request(url, (promiseErr, __, charactersReqBody) => {
-          if (promiseErr) {
-            reject(promiseErr);
-          }
-          resolve(JSON.parse(charactersReqBody).name);
-        });
-      }));
+// Main Request
+request(mainUrl, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const urlArray = JSON.parse(body).characters;
+    myAsyn(urlArray);
+  }
+});
 
-    Promise.all(charactersName)
-      .then(names => console.log(names.join('\n')))
-      .catch(allErr => console.log(allErr));
+// Sub request function
+function subRequest (url) {
+  return new Promise(function (resolve, reject) {
+    request(url, function (error2, response2, body2) {
+      if (error2) {
+        reject(error2);
+      } else {
+        resolve(JSON.parse(body2).name);
+      }
+    });
   });
+}
+
+// Asynchronous function awaits for
+// sub request resolution after each iterations
+async function myAsyn (urlArray) {
+  for (const url of urlArray) {
+    const character = await subRequest(url);
+    console.log(character);
+  }
 }
